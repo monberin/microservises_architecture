58789d6edff74cb456d0b5e9648ec58fe2e6d0ee
monberin <hnatenko@ucu.edu.ua>
2022-05-28 18:22:47 +0200

    fixed

--- facade/app.py
+++ facade/app.py
@@ -2,31 +2,54 @@ from flask import Flask, request
 import uuid
 import requests
 import random
-
+import socket
 app = Flask(__name__)
 
-messages_service = "http://localhost:9081/messages"
+messages_service = "http://127.0.0.1:9081/messages"
 
-logging_services = ["http://localhost:9082/logging",
-                    "http://localhost:9083/logging",
-                    "http://localhost:9084/logging"]
+logging_services = ["http://127.0.0.1:9082/logging",
+                    "http://127.0.0.1:9083/logging",
+                    "http://127.0.0.1:9084/logging"]
+
+def is_open(port):
+    # https://www.adamsmith.haus/python/answers/how-to-check-if-a-network-port-is-open-in-python
+    a_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    location = ("127.0.0.1", port)
+    result_of_check = a_socket.connect_ex(location)
+    a_socket.close()
+    if result_of_check == 0:
+        return True
+    return False
 
 
 @app.route('/facade', methods=['POST'])
 def post_flow():
     message = {'id': uuid.uuid4(), 'text': request.get_json()}
-    r = requests.post(random.choice(logging_services), message)
-    print(r.text)
-    print(r.status_code)
-    return r.text
+    logging_service = random.choice(logging_services)
+    if is_open(int(logging_service[17:21])):
+        print('here')
+        r = requests.post(logging_service, message)
+        print(r.text)
+        print(r.status_code)
+        return r.text
+
+    else:
+        logging_services.remove(logging_service)
+        return f'this logging service {logging_service[17:21]} was disconnected'
 
 
 @app.route('/facade', methods=['GET'])
 def get_flow():
-    logging_resp = requests.get(random.choice(logging_services)).text
-    messages_resp = requests.get(messages_service).text
-    print(str(logging_resp) + ":" + str(messages_resp))
-    return str(logging_resp) + ":" + str(messages_resp)
+    logging_service = random.choice(logging_services)
+    if is_open(int(logging_service[17:21])):
+        logging_resp = requests.get(logging_service).text
+        messages_resp = requests.get(messages_service).text
+        print(str(logging_resp) + ":" + str(messages_resp))
+        return str(logging_resp) + ":" + str(messages_resp)
+    else:
+        logging_services.remove(logging_service)
+        return 'this logging service was disconnected'
 
 
 if __name__ == '__main__':
---
+++ logging/__main__.py
@@ -0,0 +1,41 @@
+import sys
+
+from flask import Flask, request
+import hazelcast
+
+def main():
+    app = Flask(__name__)
+    print(sys.argv[2])
+    hz_client = hazelcast.HazelcastClient(cluster_name="HW3",
+                                          cluster_members=[str(sys.argv[2])])
+    print('2')
+    # "127.0.0.1:5701",
+    # "127.0.0.1:5702",
+    # "127.0.0.1:5703",
+    messages = hz_client.get_map('logging_map').blocking()
+    print('3')
+    
+    
+    @app.route('/logging', methods=['POST'])
+    def post_flow():
+        id = request.form['id']
+        text = request.form['text']
+        messages.lock(id)
+        try:
+            messages.set(id, text)
+        finally:
+            messages.unlock(id)
+        print(text)
+        print(str(messages.values()))
+        return ''
+    
+    
+    @app.route('/logging', methods=['GET'])
+    def get_flow():
+        return str(messages.values())
+
+    app.run(host='127.0.0.1', debug=True, port=int(sys.argv[1]))
+    
+if __name__ == '__main__':
+    print('logging service...')
+    main()
