d27db74696483126bcc52c495843af55efb5325a
Daryna Hnatenko <56642774+monberin@users.noreply.github.com>
2022-05-30 19:26:40 +0300

    small fix

--- writer.py
+++ writer.py
@@ -5,8 +5,6 @@ def writer():
     client = hazelcast.HazelcastClient(cluster_name="hw2",
                                            cluster_members=[
                                                "127.0.0.1:5701",
-                                               "127.0.0.1:5701",
-                                               "127.0.0.1:5701",
                                            ])
     queue = client.get_queue('bounded_queue').blocking()
 
